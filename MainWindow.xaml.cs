27f489fa3a347b470ddbb791f2aa98dbd65659a3
sztodo <57600228+sztodo@users.noreply.github.com>
2021-10-28 14:47:06 +0300

    4.10

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace Lab2
 {
     /// <summary>
@@ -20,9 +19,177 @@ namespace Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        PizzaType selectedPizza;
+        private PizzaMachine myPizzaMachine;
+        private int mMargheritaPizza;
+        private int mPepperoniPizza;
+        private int mVeggiePizza;
+        private int mQuattroStagioniPizza;
+        private int mCanibalePizza;
+        private void margPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = true;
+            pepPizzaMenuItem.IsChecked = false;
+            vegPizzaMenuItem.IsChecked = false;
+            quatPizzaMenuItem.IsChecked = false;
+            canPizzaMenuItem.IsChecked = false;
+            myPizzaMachine.MakePizzas(PizzaType.Margherita);
+        }
+        private void pepPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = false;
+            pepPizzaMenuItem.IsChecked = true;
+            vegPizzaMenuItem.IsChecked = false;
+            quatPizzaMenuItem.IsChecked = false;
+            canPizzaMenuItem.IsChecked = false;
+            myPizzaMachine.MakePizzas(PizzaType.Pepperoni);
+        }
+        private void vegPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = false;
+            pepPizzaMenuItem.IsChecked = false;
+            vegPizzaMenuItem.IsChecked = true;
+            quatPizzaMenuItem.IsChecked = false;
+            canPizzaMenuItem.IsChecked = false;
+            myPizzaMachine.MakePizzas(PizzaType.Veggie);
+        }
+        private void quatPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = false;
+            pepPizzaMenuItem.IsChecked = false;
+            vegPizzaMenuItem.IsChecked = false;
+            quatPizzaMenuItem.IsChecked = true;
+            canPizzaMenuItem.IsChecked = false;
+            myPizzaMachine.MakePizzas(PizzaType.Quattro_Stagioni);
+        }
+        private void canPizzaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            margPizzaMenuItem.IsChecked = false;
+            pepPizzaMenuItem.IsChecked = false;
+            vegPizzaMenuItem.IsChecked = false;
+            quatPizzaMenuItem.IsChecked = false;
+            canPizzaMenuItem.IsChecked = true;
+            myPizzaMachine.MakePizzas(PizzaType.Canibale);
+        }
+        private void PizzaCompleteHandler()
+        {
+            switch (myPizzaMachine.Ingredients)
+            {
+                case PizzaType.Margherita:
+                    mMargheritaPizza++;
+                    txtMargheritaPizza.Text = mMargheritaPizza.ToString();
+                    break;
+                case PizzaType.Pepperoni:
+                    mPepperoniPizza++;
+                    txtPepperoniPizza.Text = mPepperoniPizza.ToString();
+                    break;
+                case PizzaType.Veggie:
+                    mVeggiePizza++;
+                    txtVeggiePizza.Text = mVeggiePizza.ToString();
+                    break;
+                case PizzaType.Quattro_Stagioni:
+                    mQuattroStagioniPizza++;
+                    txtQuatroPizza.Text = mQuattroStagioniPizza.ToString();
+                    break;
+                case PizzaType.Canibale:
+                    mCanibalePizza++;
+                    txtCanibalePizza.Text = mCanibalePizza.ToString();
+                    break;
+
+            }
+
+        }
+        KeyValuePair<PizzaType, double>[] PriceList =
+            {
+                new KeyValuePair<PizzaType, double>(PizzaType.Margherita, 21),
+                new KeyValuePair<PizzaType, double>(PizzaType.Pepperoni, 23),
+                new KeyValuePair<PizzaType, double>(PizzaType.Veggie, 20),
+                new KeyValuePair<PizzaType, double>(PizzaType.Quattro_Stagioni, 27),
+                new KeyValuePair<PizzaType, double>(PizzaType.Canibale, 30)
+            };
+        private int ValidateQuantity(PizzaType selectedPizza)
+        {
+            int q = int.Parse(txtQuantity.Text);
+            int r = 1;
+            switch (selectedPizza)
+            {
+                case PizzaType.Margherita:
+                    if (q > mMargheritaPizza)
+                        r = 0;
+                    break;
+                case PizzaType.Pepperoni:
+                    if (q > mPepperoniPizza)
+                        r = 0;
+                    break;
+                case PizzaType.Veggie:
+                    if (q > mVeggiePizza)
+                        r = 0;
+                    break;
+                case PizzaType.Quattro_Stagioni:
+                    if (q > mQuattroStagioniPizza)
+                        r = 0;
+                    break;
+                case PizzaType.Canibale:
+                    if (q > mCanibalePizza)
+                        r = 0;
+                    break;
+            }
+            return r;
+        }
+
+
         public MainWindow()
         {
+            
             InitializeComponent();
+        }
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            myPizzaMachine = new PizzaMachine();
+            myPizzaMachine.PizzaComplete += new PizzaMachine.PizzaCompleteDelegate(PizzaCompleteHandler);
+            cmbType.ItemsSource = PriceList;
+            cmbType.DisplayMemberPath = "Key";
+            cmbType.SelectedValuePath = "Value";
+        }
+        private void stopMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            myPizzaMachine.Enabled = false;
+        }
+        private void exitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtQuantity_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (!(e.Key >= Key.D0 && e.Key <= Key.D9))
+            {
+                MessageBox.Show("Numai cifre se pot introduce!", "Input Error", MessageBoxButton.OK,
+               MessageBoxImage.Error);
+            }
+        }
+        private void cmbType_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            txtPrice.Text = cmbType.SelectedValue.ToString();
+            KeyValuePair<PizzaType, double> selectedEntry = (KeyValuePair<PizzaType, double>)
+            cmbType.SelectedItem;
+            selectedPizza = selectedEntry.Key;
+        }
+        private void btnAddToSale_Click(object sender, RoutedEventArgs e)
+        {
+            if (ValidateQuantity(selectedPizza) > 0)
+            {
+                lstSale.Items.Add(txtQuantity.Text + " " + selectedPizza.ToString() + ":" +
+               txtPrice.Text + " " + double.Parse(txtQuantity.Text) * double.Parse(txtPrice.Text));
+            }
+            else
+            {
+                MessageBox.Show("Cantitatea introdusa nu este disponibila in stoc!");
+            }
+        }
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            lstSale.Items.Remove(lstSale.SelectedItem);
         }
     }
 }
--- PizzaMachine.cs
+++ PizzaMachine.cs
@@ -1,17 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Windows.Threading;
 
 namespace Lab2
+
 {
-    class PizzaMachine
+    class PizzaMachine : Component
     {
+        public PizzaMachine()
+        {
+            InitializeComponent();
+        }
         private PizzaType mIngredients;
         public PizzaType Ingredients
         {
             get { return mIngredients; }
             set { mIngredients = value; }
         }
+        private System.Collections.ArrayList mPizzas = new System.Collections.ArrayList();
+        public Pizza this[int Index]
+        {
+            get
+            {
+                return (Pizza)mPizzas[Index];
+            }
+            set
+            {
+                mPizzas[Index] = value;
+            }
+        }
+
+        public delegate void PizzaCompleteDelegate();
+        public event PizzaCompleteDelegate PizzaComplete;
+        DispatcherTimer pizzaBakeTimer;
+        private void pizzaBakeTimer_Tick(object sender, EventArgs e)
+        {
+            Pizza aPizza = new Pizza(this.Ingredients);
+            mPizzas.Add(aPizza);
+            PizzaComplete();
+        }
+        private void InitializeComponent()
+        {
+            this.pizzaBakeTimer = new DispatcherTimer();
+            this.pizzaBakeTimer.Tick += new System.EventHandler(this.pizzaBakeTimer_Tick);
+        }
+        public bool Enabled
+        {
+            set
+            {
+                pizzaBakeTimer.IsEnabled = value;
+            }
+        }
+        public int Interval
+        {
+            set
+            {
+                pizzaBakeTimer.Interval = new TimeSpan(0, 0, value);
+            }
+        }
+        public void MakePizzas(PizzaType dIngredients)
+        {
+            Ingredients = dIngredients;
+
+            switch (Ingredients)
+            {
+                case PizzaType.Canibale: Interval = 3; break;
+                case PizzaType.Margherita: Interval = 2; break;
+                case PizzaType.Pepperoni: Interval = 5; break;
+                case PizzaType.Quattro_Stagioni: Interval = 7; break;
+                case PizzaType.Veggie: Interval = 4; break;
+            }
+            pizzaBakeTimer.Start();
+        }
+
     }
     enum PizzaType
     {
@@ -32,7 +95,7 @@ namespace Lab2
             }
             set => mIngredients = value;
         }
-            private float mPrice = .50F;
+        private float mPrice = .50F;
         public float Price
         {
             get
@@ -50,7 +113,8 @@ namespace Lab2
             get { return mTimeofCreation; }
         }
         public Pizza(PizzaType aIngredients)
-        { mTimeofCreation = DateTime.Now;
+        {
+            mTimeofCreation = DateTime.Now;
             mIngredients = aIngredients;
         }
     }
